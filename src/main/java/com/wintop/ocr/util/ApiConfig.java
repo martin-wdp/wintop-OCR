e054dfd8cda365cb378c7f18a6647732d716afd6
wangdapeng <wdpxl@sina.com>
2018-01-12 10:54:44 +0800

    商城接口调整

---
+++ src/main/java/com/wintop/ocr/util/ApiConfig.java
@@ -0,0 +1,10 @@
+package com.wintop.ocr.util;
+
+public class ApiConfig {
+
+    public static final String ALIBABA_host = "https://dm-51.data.aliyun.com";
+    public static final String ALIBABA_path = "/rest/160601/ocr/ocr_idcard.json";
+    public static final String ALIBABA_method = "POST";
+    public static final String ALIBABA_appcode = "7f18f9b3524b414cb7d47fca8637f571";
+
+}
